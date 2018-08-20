6f8720a6568f82df6f957c94dbe44addbbcceaae
AdGlad <bagladstone@bigpond.com>
2018-08-20 07:42:14 +0000

    fixing bug in rekognition

--- app/controllers/identifies_controller.rb
+++ app/controllers/identifies_controller.rb
@@ -22,8 +22,10 @@ resp = client.search_faces_by_image({
 end
 
 def show
+#imagefile="uploads/image/picture/4/Jake_Trbojevic.JPG"
+imagefile="uploads/image/picture/4/Daily_Cherry-Evans.jpg"
 #imagefile="uploads/image/picture/" + @image.user_id.to_s + "/Jake_Trbojevic.JPG"
-imagefile="uploads/image/picture/" + @image.user_id.to_s + "/" + @image.picture.to_s
+#imagefile="uploads/image/picture/" + @image.user_id.to_s + "/" + @image.picture.to_s
 search_faces_by_image("ManlySeaEagles","idoraapp",imagefile)
 
 #@image.matchid="'Cherry-Evans"
