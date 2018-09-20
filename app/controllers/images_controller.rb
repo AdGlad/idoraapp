a3a442bf0e723f265c22c8f48fd0bd43e04175d8
EC2 Default User <ec2-user@ip-172-31-22-161.eu-west-1.compute.internal>
2018-09-20 01:48:22 +0000

    Moved matching logic into image/show.html.erb

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,12 +1,81 @@
 class ImagesController < ApplicationController
+  require 'aws-sdk'
   before_action :set_image, only: [:check, :show, :edit, :update, :destroy]
 
-  # GET /images
-  # GET /images.json
-  def index
-    @images = Image.where(user_id: current_user.id).paginate(page: params[:page], per_page: 5)
+def detect_labels(collectionname,sourcebucketname,sourcefilename)
+client = Aws::Rekognition::Client.new
+
+resp = client.detect_labels({
+  image: {
+    s3_object: {
+        bucket: sourcebucketname,
+        name: sourcefilename,
+    },
+  },
+  max_labels:10
+})
+
+puts "*********************"
+puts resp.to_h
+puts "*********************"
+
+resp.labels.each do |label|
+  puts "#{label.name}-#{label.confidence.to_i}"
+end
+
+@image.scene_matched= resp.to_h
+
+end
+
+def search_faces_by_image(collectionname,sourcebucketname,sourcefilename)
+  client = Aws::Rekognition::Client.new
+  
+  resp = client.search_faces_by_image({
+    collection_id: collectionname,
+    face_match_threshold: 60,
+    image: {
+      s3_object: {
+          bucket: sourcebucketname,
+          name: sourcefilename,
+      },
+    },
+    max_faces:10
+    })
+  resp.face_matches.each do |face_matches|
+    puts "#{face_matches.face.external_image_id}-#{face_matches.face.confidence.to_i}"
   end
 
+  matched_faceid= resp.face_matches[0].face.face_id
+  puts "matched_faceid" + matched_faceid.to_s
+ image_id = @image.id
+  if not matched_faceid.empty?
+    facematchname=Identity.where(face_id: matched_faceid).first
+    #@image.matchid=resp.face_matches[0].face.external_image_id 
+    if not ImageIdentity.exists?(image_id: image_id, identity_id: facematchname.id)
+      @image.identities << facematchname
+      puts "Creating match record"
+    else
+      puts "Match record already exists"
+    end
+
+    if not facematchname.name.empty?
+      @image.matchid= facematchname.name
+      @image.faces_matched= resp.to_h
+      #@imagematch = Imagematch.new(name: @image.matchid, desc: resp.face_matches[0].face.external_image_id, resp: resp.to_h, image_id: @image.id) 
+      #@imagematch.save
+    end
+    @image.save
+
+  end
+end
+  #
+    # GET /images
+    # GET /images.json
+  
+def index
+  @images = Image.where(user_id: current_user.id).paginate(page: params[:page], per_page: 5)
+end
+  
   # GET /images/1
   # GET /images/1.json
   def show
@@ -74,6 +143,23 @@ class ImagesController < ApplicationController
         format.json { render json: @image.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def match
+      @image = Image.find(params[:id])
+      @user = User.find(@image.user_id)
+      collectionid=@user.collectionid
+      picture = @image.picture.path.split("/").last
+      imagefile="uploads/image/picture/" +  Rails.env + "/" + @image.user_id.to_s + "/" + picture.to_s
+      puts " collectionid" + collectionid
+      puts imagefile
+  ##search_faces_by_image("ManlySeaEagles","idorabucket",imagefile)
+      search_faces_by_image(collectionid,"idorabucket",imagefile)
+  #detect_labels(collectionid,"idorabucket",imagefile)
+  ##  recognize_celebrities("idorabucket",imagefile)
+  ##@image.matchid="'Cherry-Evans"
+  #@image.save
+      render "show"
   end
 
   private
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,19 +1,58 @@
 class Image < ApplicationRecord
+  require 'aws-sdk'
   belongs_to :user
   #has_many :images
   has_many :image_identities
   has_many :identities, through: :image_identities
   mount_uploader :picture, PictureUploader
-
+  #after_commit :search_faces_by_image
   validate :picture_size
   validates :name, presence: true
 
 private
-
   def picture_size
     if picture.size > 5.megabytes
     errors.add(:picture, "should be less than 5MB")
     end
   end
 
+  def search_faces_by_image
+    @user=User.find(self.user_id)
+    collectionid=@user.collectionid 
+    client = Aws::Rekognition::Client.new
+    bucketname= "idorabucket"
+    #imagefile="uploads/image/picture/" +  Rails.env + "/" + self.user_id.to_s + "/" + picture.to_s
+    imagefile="uploads/image/picture/development/3/player16.jpg"
+    resp = client.search_faces_by_image({
+                   collection_id: collectionid,
+                   face_match_threshold: 60,
+                   image: {
+                       s3_object: {
+                           bucket: bucketname,
+                           name: imagefile,
+                                  },
+                          },
+                     max_faces:10
+                   })
+
+    resp.face_matches.each do |face_matches|
+      puts "#{face_matches.face.external_image_id}-#{face_matches.face.confidence.to_i}"
+    end
+
+    matched_faceid= resp.face_matches[0].face.face_id
+  
+    if not matched_faceid.empty?
+      facematchname=Identity.where(face_id: matched_faceid).first
+      if not ImageIdentity.exists?(identity_id: facematchname.id)
+        self.identities << facematchname
+      else
+        puts "Match record already exists"
+      end
+      if not facematchname.name.empty?
+        self.matchid= facematchname.name
+        self.faces_matched= resp.to_h
+        self.save
+      end
+    end
+end
 end
